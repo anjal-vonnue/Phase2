4b2911e04dc72b8644808adfd3ab856e893b443a
anjal-vonnue <anjal.biju@vonnue.com>
2026-09-18 10:45:09 +0530

    feat: added auth services

---
+++ day-16/src/module/auth/auth.service.ts
@@ -0,0 +1,89 @@
+import "dotenv/config";
+import bcrypt from "bcrypt";
+import { UserRole } from "../../generated/prisma/enums.js";
+import prisma from "../../db/prisma.js";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+if (!JWT_SECRET) {
+  throw new Error("JWT_SECRET is not defined");
+}
+
+export async function registerUser(
+  name: string,
+  email: string,
+  password: string,
+  role: UserRole,
+) {
+  const passwordHash = await bcrypt.hash(password, 12);
+  const user = await prisma.user.create({
+    data: {
+      name: name,
+      email: email,
+      passwordHash: passwordHash,
+      role: role,
+    },
+    select: {
+      id: true,
+      name: true,
+      role: true,
+      createdAt: true,
+    },
+  });
+
+  const token = jwt.sign(
+    {
+      userId: user.id,
+      role: user.role,
+    },
+    JWT_SECRET!,
+    {
+      expiresIn: "12h",
+    },
+  );
+
+  return {
+    user,
+    token,
+  };
+}
+
+export async function loginUser(email: string, password: string) {
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+
+  if (!user) {
+    throw new Error("INVALID CREDENTIALS");
+  }
+
+  const validPassword = await bcrypt.compare(password, user.passwordHash);
+
+  if (!validPassword) {
+    throw new Error("INVALID CREDENTIALS");
+  }
+
+  const token = jwt.sign(
+    {
+      userId: user.id,
+      role: user.role,
+    },
+    JWT_SECRET!,
+    {
+      expiresIn: "12h",
+    },
+  );
+
+  return {
+    user: {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      createdAt: user.createdAt,
+    },
+    token,
+  };
+}
