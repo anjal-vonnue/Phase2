2b5c22358adf0cdd13d6338ac3d23be4b78a1039
anjal-vonnue <anjal.biju@vonnue.com>
2026-09-18 13:25:27 +0530

    feat: protected the tickets routes

--- day-16/src/app.ts
+++ day-16/src/app.ts
@@ -2,12 +2,13 @@ import express from "express";
 import router from "./routes/ticketRoutes.js";
 import authRoute from "./module/auth/auth.route.js";
 import userRoute from "./module/me/me.route.js";
+import { authenticate } from "./middlewares/auth.middleware.js";
 
 const app = express();
 
 app.use(express.json());
 
-app.use(router);
+app.use(authenticate, router);
 app.use("/auth", authRoute);
 app.use(userRoute);
 
