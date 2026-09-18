2d87b78ed44dbaf87a938c7488357c5eeeddc13e
anjal-vonnue <anjal.biju@vonnue.com>
2026-09-18 11:10:23 +0530

    feat: added register controller

--- day-16/src/module/auth/auth.controller.ts
+++ day-16/src/module/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from "express";
-import { registerUser } from "./auth.service.js";
+import { loginUser, registerUser } from "./auth.service.js";
 
 export async function register(req: Request, res: Response) {
   try {
@@ -13,9 +13,33 @@ export async function register(req: Request, res: Response) {
 
     return res.status(201).json(result);
   } catch (error) {
+    console.error(error);
     if (error instanceof Error && error.message === "EMAIL ALREADY EXISTS") {
       return res.json(409).json({
         message: "unable to create account",
+      });
+    }
+    return res.status(500).json({ message: "internal server error" });
+  }
+}
+
+export async function login(req: Request, res: Response) {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "email and password are required",
+      });
+    }
+
+    const result = await loginUser(email, password);
+
+    return res.status(200).json(result);
+  } catch (error) {
+    console.error(error);
+    if (error instanceof Error && error.message === "INVALID CREDENTIALS") {
+      return res.status(401).json({
+        message: "invalid credential",
       });
     }
     return res.status(500).json({ message: "internal server error" });
