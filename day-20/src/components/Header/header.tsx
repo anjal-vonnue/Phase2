eee063dff31ad63ce9a3d7b195edbf8f1fd17366
anjal-vonnue <anjal.biju@vonnue.com>
2026-09-21 17:35:28 +0530

    feat: added a sample header

--- day-20/src/components/Header/header.tsx
+++ day-20/src/components/Header/header.tsx
@@ -1,7 +1,38 @@
+import "./header.css";
+
+const links = [
+  {
+    id: 1,
+    name: "Home",
+    href: "/home",
+  },
+  {
+    id: 2,
+    name: "Projects",
+    href: "/projects",
+  },
+  {
+    id: 3,
+    name: "Issues",
+    href: "/issues",
+  },
+];
+
 export default function HeaderComponent() {
   return (
     <>
-      <h2>Header</h2>
+      <header className="header">
+        <div>Project Management</div>
+        <div>
+          <nav className="nav">
+            {links.map((link) => (
+              <a href={link.href} key={link.id}>
+                {link.name}
+              </a>
+            ))}
+          </nav>
+        </div>
+      </header>
     </>
   );
 }
